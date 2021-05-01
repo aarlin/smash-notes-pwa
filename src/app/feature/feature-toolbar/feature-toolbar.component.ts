42e92436382ca3687223435a69afe8f3465e1115
Aaron Lin <aaronlin256@gmail.com>
2021-05-01 00:29:55 -0400

    add character assets

--- src/app/feature/feature-toolbar/feature-toolbar.component.ts
+++ src/app/feature/feature-toolbar/feature-toolbar.component.ts
@@ -1,5 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Character {
+  name: string;
+}
+
+enum PortraitSize {
+  FULL = 'full',
+  SMALL = 'small_horizontal',
+  THUMBNAIL_HORIZONTAL = 'thumbnail_horizontal',
+}
+
+enum PortraitImageExtension {
+  WEBP = 'webp',
+  PNG = 'png',
+}
+
+
 @Component({
   selector: 'app-feature-toolbar',
   templateUrl: './feature-toolbar.component.html',
@@ -9,8 +25,17 @@ export class FeatureToolbarComponent implements OnInit {
 
   colorTheme = ["", "secondary", "dark"]
 
+  selectedPlayerImage = 'assets/portraits/thumbnail_horizontal/ico_dlc_cs.svg';
+  selectedEnemyImage = 'assets/portraits/thumbnail_horizontal/ico_dlc_cs.svg';
+
+  portraitSize: PortraitSize = PortraitSize.SMALL;
+  portraitImageExtension: PortraitImageExtension = PortraitImageExtension.WEBP;
+
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.selectedPlayerImage = `assets/portraits/${this.portraitSize}/homura.${this.portraitImageExtension}`;
+    this.selectedEnemyImage = `assets/portraits/${this.portraitSize}/homura.${this.portraitImageExtension}`;
+  }
 
 }
