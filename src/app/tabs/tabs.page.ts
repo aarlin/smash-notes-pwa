d85e1135428eec511f32fcb59e62597b1ff758e3
Aaron Lin <aaronlin256@gmail.com>
2021-05-04 17:39:20 -0400

    use ssbu svg for navigation

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
   styleUrls: ['tabs.page.scss']
 })
-export class TabsPage {
+export class TabsPage implements OnInit {
+
+  homeIcon: string;
+  discoverIcon: string;
+  fighterIcon: string;
 
   constructor() {}
+
+  ngOnInit() {
+    this.homeIcon = 'assets/navigation/ico_top_g.svg';
+    this.discoverIcon = 'assets/navigation/ico_howtoplay_g.svg';
+    this.fighterIcon = 'assets/navigation/ico_fighter_g.svg';
+  }
+
+  getChosenFighter(fighter: string) {
+    if (fighter) {
+      return `assets/stock-icons/svg/${fighter}.svg`;
+    } else {
+      return `assets/navigation/ico_fighter_g.svg`;
+    }
+  }
+
+  displayFighterModal() {
+    this.fighterIcon = 'assets/stock-icons/svg/bayonetta.svg';
+  }
 
 }
