354b0d6811d8c551704be09983a283aa82d1b5b4
Aaron Lin <aaronlin256@gmail.com>
2021-05-04 21:02:12 -0400

    add search functionality to select fighter modal

--- src/app/feature/feature-character-select-modal/character-select-modal/character-select-modal.component.ts
+++ src/app/feature/feature-character-select-modal/character-select-modal/character-select-modal.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FighterService } from 'src/app/services/fighter.service';
+
+interface Fighter {
+  name?: string;
+  appearsIn?: string[];
+  url?: string;
+}
 
 @Component({
   selector: 'smash-character-select-modal',
@@ -8,11 +15,41 @@ import { Component, OnInit } from '@angular/core';
 export class CharacterSelectModalComponent implements OnInit {
 
   fighterIcon: string;
+  pageToLoadNext: number = 1;
+  pageSize: number = 83;
+
+  fighters: Fighter[] = [];
+  backupFighters: Fighter[] = [];
+  searchTerm: string = '';
   
-  constructor() { }
+  constructor(private fighterService: FighterService) {
+    
+  }
 
   ngOnInit() {
     this.fighterIcon = 'assets/navigation/ico_fighter_g.svg';
+
+    this.fighterService.load(this.pageToLoadNext, this.pageSize)
+    .subscribe((fighters: Fighter[]) => {
+      this.fighters = fighters.map(fighter => {
+        fighter.url = `assets/stock-icons/svg/${fighter.name}.svg`;
+        return fighter;
+      });
+      this.backupFighters = this.fighters;
+    });
+  }
+
+  setFilteredItems(event: any) {
+    this.fighters = this.backupFighters;
+    const searchTerm = event.srcElement.value;
+
+    if (!searchTerm) {
+      return;
+    }
+
+    this.fighters = this.fighters.filter(fighter => {
+      return fighter?.name?.toLowerCase().startsWith(searchTerm.toLowerCase());
+    });
   }
 
 }
--- src/app/feature/feature-character-select-modal/feature-character-select-modal.component.ts
+++ src/app/feature/feature-character-select-modal/feature-character-select-modal.component.ts
@@ -2,14 +2,19 @@ import { Component, OnInit } from '@angular/core';
 import { AlertController, ModalController } from '@ionic/angular';
 import { CharacterSelectModalComponent } from './character-select-modal/character-select-modal.component';
 
+
+
+
+
 @Component({
   selector: 'smash-feature-character-select-modal',
   templateUrl: './feature-character-select-modal.component.html',
   styleUrls: ['./feature-character-select-modal.component.scss'],
 })
-export class FeatureCharacterSelectModalComponent {
+export class FeatureCharacterSelectModalComponent  {
 
-  constructor(public alertController: AlertController, public modalController: ModalController) {}
+
+  constructor( public alertController: AlertController, public modalController: ModalController) {}
 
 
   async presentModal() {
@@ -20,89 +25,6 @@ export class FeatureCharacterSelectModalComponent {
       cssClass: 'character-select-modal'
     });
     return await modal.present();
-  }
-
-  async presentAlertPrompt() {
-    const alert = await this.alertController.create({
-      cssClass: 'my-custom-class',
-      header: 'Prompt!',
-      inputs: [
-        {
-          name: 'name1',
-          type: 'text',
-          placeholder: 'Placeholder 1'
-        },
-        {
-          name: 'name2',
-          type: 'text',
-          id: 'name2-id',
-          value: 'hello',
-          placeholder: 'Placeholder 2'
-        },
-        // multiline input.
-        {
-          name: 'paragraph',
-          id: 'paragraph',
-          type: 'textarea',
-          placeholder: 'Placeholder 3'
-        },
-        {
-          name: 'name3',
-          value: 'http://ionicframework.com',
-          type: 'url',
-          placeholder: 'Favorite site ever'
-        },
-        // input date with min & max
-        {
-          name: 'name4',
-          type: 'date',
-          min: '2017-03-01',
-          max: '2018-01-12'
-        },
-        // input date without min nor max
-        {
-          name: 'name5',
-          type: 'date'
-        },
-        {
-          name: 'name6',
-          type: 'number',
-          min: -5,
-          max: 10
-        },
-        {
-          name: 'name7',
-          type: 'number'
-        },
-        {
-          name: 'name8',
-          type: 'password',
-          placeholder: 'Advanced Attributes',
-          cssClass: 'specialClass',
-          attributes: {
-            maxlength: 4,
-            inputmode: 'decimal'
-          }
-        }
-      ],
-      buttons: [
-        {
-          text: 'Cancel',
-          role: 'cancel',
-          cssClass: 'secondary',
-          handler: () => {
-            console.log('Confirm Cancel');
-          }
-        }, {
-          text: 'Ok',
-          handler: () => {
-            console.log('Confirm Ok');
-          }
-        }
-      ]
-    });
-
-    await alert.present();
   }
 
 
