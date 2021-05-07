877316687caa1ccfbc5b8b76cde8256a443977ee
Aaron Lin <aaronlin256@gmail.com>
2021-05-07 18:21:38 -0400

    move html data into ts file

--- src/app/feature/feature-fighter-notes/feature-fighter-notes.component.ts
+++ src/app/feature/feature-fighter-notes/feature-fighter-notes.component.ts
@@ -2,7 +2,18 @@ import { Component, OnInit } from '@angular/core';
 
 interface Fighter {
   name?: string;
+}
 
+interface Note {
+  groupName: string,
+  enemy: string,
+  title: string,
+  body: string,
+  enemyImage?: string
+}
+
+interface ChunkedData {
+  [key: string]: Note[];
 }
 
 @Component({
@@ -16,11 +27,61 @@ export class FeatureFighterNotesComponent implements OnInit {
   background: string;
   fighterImage: string;
 
+  notes: Note[] = [
+    {
+      groupName: "Stage Counterpick",
+      enemy: "bayonetta",
+      title: "Lylatt",
+      body: "Choose lyatt for no ceiling"
+    },
+    {
+      groupName: "Stage Counterpick",
+      enemy: "sephiroth",
+      title: "Yoshi Island",
+      body: "Choose lyatt for no ceiling"
+    },
+    {
+      groupName: "Kill Confirm",
+      enemy: "donkey_kong",
+      title: "60%",
+      body: "Down air to up smash"
+    },
+    {
+      groupName: "Combo",
+      enemy: "donkey_kong",
+      title: "60%",
+      body: "down throw to bair"
+    },
+    {
+      groupName: "Combo",
+      enemy: "captain_falcon",
+      title: "60%",
+      body: "down throw to fair"
+    }
+  ];
+
+  chunkedData: ChunkedData = this.chunkByGroup(this.notes);
+
   constructor() { }
 
   ngOnInit() {
     let sampleFighter: Fighter = { name: 'homura' }
     this.setBackgroundImage(sampleFighter);
+    let chunkedData = this.chunkByGroup(this.notes);
+    console.log(chunkedData);
+  }
+
+  chunkByGroup(notes: Note[]) {
+    let groups = {};
+    for (let note of notes) {
+      console.log(note, groups[note.groupName])
+      if (groups[note.groupName] === undefined) {
+        groups[note.groupName] = []
+      } 
+      note.enemyImage = `https://www.smashbros.com/assets_v2/img/fighter/thumb_a/${note.enemy}.png`
+      groups[note.groupName].push(note);
+    }
+    return groups;
   }
 
   setBackgroundImage(fighter: Fighter) {
