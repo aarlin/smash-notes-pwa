f061525091d4467ddb8a03770379cba517c33909
Aaron Lin <aaronlin256@gmail.com>
2021-05-28 04:08:11 -0400

    remove unneccessary files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   constructor() {}
+
+  ngOnInit() {
+    this.hideSpinner();
+  }
+
+  hideSpinner() {
+    document.getElementById('nb-global-spinner').style.display = 'none';
+  }
 }
