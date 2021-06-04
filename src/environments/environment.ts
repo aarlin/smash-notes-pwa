1cb676f1b0f0e78357d7822936bd0e8925cca697
Aaron Lin <aaronlin256@gmail.com>
2021-06-04 13:41:48 -0400

    add back environment.ts after research on api key safety

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyA4YeYev8uM0I6ea1iSnhH1njpyu8DOT0k",
+    authDomain: "smash-notes-21d97.firebaseapp.com",
+    projectId: "smash-notes-21d97",
+    storageBucket: "smash-notes-21d97.appspot.com",
+    messagingSenderId: "342339219115",
+    appId: "1:342339219115:web:4b1047dbd8e46aeefd11ee",
+    measurementId: "G-H1FR2RHDQ1"
+  }
+};
