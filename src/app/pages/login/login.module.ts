c405f8bfb5b533199d261f68ac6e224af84a6164
Aaron Lin <aaronlin256@gmail.com>
2021-06-15 02:51:01 -0400

    add library for firebase ui login

--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -7,6 +7,7 @@ import { IonicModule } from '@ionic/angular';
 import { LoginPageRoutingModule } from './login-routing.module';
 
 import { LoginPage } from './login.page';
+import { FirebaseUIModule } from 'firebaseui-angular';
 
 @NgModule({
   imports: [
@@ -14,7 +15,8 @@ import { LoginPage } from './login.page';
     FormsModule,
     IonicModule,
     LoginPageRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    FirebaseUIModule
   ],
   declarations: [LoginPage]
 })
